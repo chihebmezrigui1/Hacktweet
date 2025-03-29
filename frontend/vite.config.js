7b4099f58b8a48e8e7ce74e8b95150166d475b05
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-29 01:32:55 +0100

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ajout de cette ligne pour utiliser des chemins relatifs
+  base: '/', // Utilisez un slash simple pour les chemins absolus
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   server: {
     proxy: {
       '/api': {

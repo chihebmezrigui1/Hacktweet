06c710f35145026d24d5a281cbce503fe8fcf474
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-29 15:30:49 +0100

    fix generatetoken

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,7 +5,7 @@ import { protectRoute } from "../middleware/protectRoute.js";
 const router = express.Router();
 
 
-router.get("/me", getMe);
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);

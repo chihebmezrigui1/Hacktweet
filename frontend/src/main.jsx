dd33d3a803eae18e3c398557500c38ace917cd4c
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-29 01:58:24 +0100

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
@@ -15,10 +15,10 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<HashRouter>
+		<BrowserRouter>
 			<QueryClientProvider client={queryClient}>
 				<App />
 			</QueryClientProvider>
-		</HashRouter>
+		</BrowserRouter>
 	</React.StrictMode>
 );

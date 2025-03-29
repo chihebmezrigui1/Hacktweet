68ea4b5af443b675ce47d33f148c4ca64ca32239
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-29 17:31:21 +0100

    allow credentails for all

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -13,6 +13,7 @@ import { Camera, Smile } from "lucide-react";
 import LoadingSpinner from "./LoadingSpinner";
 import { formatPostDate } from "../../utils/date";
 import { API_URL } from "../../API";
+import axios from "axios";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -238,7 +239,7 @@ const captureEmotion = async () => {
 	const { mutate: likePost, isPending: isLiking } = useMutation({
 		mutationFn: async () => {
 			try {
-				const res = await fetch(`/api/posts/like/${post._id}`, {
+				const res = await axios.get(`/api/posts/like/${post._id}`, {
 					method: "POST",
 					credentials: 'include'
 				});

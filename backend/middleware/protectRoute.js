7e545541023217b6a3cc423ec371752a81e7bed7
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-30 01:26:16 +0100

    fix logout

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -28,10 +28,14 @@
 //     res.status(401).json({ error: "Unauthorized - Invalid Token" });
 //   }
 // };
+
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
 export const protectRoute = async (req, res, next) => {
   try {
     // Vérification de la présence du token dans les cookies
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt; 
 
     if (!token) {
       return res.status(401).json({ error: "Unauthorized - No Token Provided" });
@@ -40,11 +44,13 @@ export const protectRoute = async (req, res, next) => {
     // Vérification de la validité du token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Si le token est valide, nous extrayons le userId
-    const userId = decoded.userId;
+    // Si le token est invalide ou ne peut pas être décodé
+    if (!decoded) {
+      return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+    }
 
     // Vérification si l'utilisateur associé au token existe dans la base de données
-    const user = await User.findById(userId);
+    const user = await User.findById(decoded.userId);
 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
@@ -59,13 +65,12 @@ export const protectRoute = async (req, res, next) => {
   } catch (error) {
     // Si une erreur survient, afficher le message d'erreur
     console.error("Error in protectRoute middleware:", error.message);
-
-    // Si une erreur est liée au token, renvoyer une erreur 401 pour accès non autorisé
+    
+    // Si une erreur est capturée, renvoyer une erreur 401 pour accès non autorisé
     if (error instanceof jwt.JsonWebTokenError) {
       return res.status(401).json({ error: "Unauthorized - Invalid or Expired Token" });
+    } else {
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-
-    // Pour toutes autres erreurs, envoyer une erreur 500
-    return res.status(500).json({ error: "Internal Server Error" });
   }
 };

2e6daf73c3282c2af77574a94723d86c7775df3d
chihab.mezrigui <chihab.mezrigui@rdiup.com>
2025-03-31 23:29:22 +0200

    fix issues phone

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -77,6 +77,7 @@ const LoginPage = () => {
 							name='username'
 							onChange={handleInputChange}
 							value={formData.username}
+							autoComplete="off"
 						/>
 					</label>
 
@@ -89,6 +90,7 @@ const LoginPage = () => {
 							name='password'
 							onChange={handleInputChange}
 							value={formData.password}
+							autoComplete="off"
 						/>
 					</label>
 					<button 

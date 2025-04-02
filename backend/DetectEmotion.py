2d8a17803d6b973fce70de9a2f8e55ac59770798
Chihab Mezrigui <99685119+chihebmezrigui1@users.noreply.github.com>
2025-04-02 10:26:47 +0200

    Update DetectEmotion.py

--- backend/DetectEmotion.py
+++ backend/DetectEmotion.py
@@ -185,5 +185,6 @@ def home():
     """
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    import os
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True)
